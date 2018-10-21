29dbfaa936f361f73447b1960eb48878499a81a2
Lawrence <e0176571@u.nus.edu>
2018-10-22 03:05:15 +0800

    added getter and setter methods for employee.java

--- HotelReservationSystem/HotelReservationSystemLibrary/src/entity/Employee.java
+++ HotelReservationSystem/HotelReservationSystemLibrary/src/entity/Employee.java
@@ -83,5 +83,145 @@ public class Employee implements Serializable {
     public String toString() {
         return "entity.Employee[ id=" + employeeId + " ]";
     }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the identificationNum
+     */
+    public String getIdentificationNum() {
+        return identificationNum;
+    }
+
+    /**
+     * @param identificationNum the identificationNum to set
+     */
+    public void setIdentificationNum(String identificationNum) {
+        this.identificationNum = identificationNum;
+    }
+
+    /**
+     * @return the contactNum
+     */
+    public String getContactNum() {
+        return contactNum;
+    }
+
+    /**
+     * @param contactNum the contactNum to set
+     */
+    public void setContactNum(String contactNum) {
+        this.contactNum = contactNum;
+    }
+
+    /**
+     * @return the addressLine1
+     */
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    /**
+     * @param addressLine1 the addressLine1 to set
+     */
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    /**
+     * @return the addressLine2
+     */
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    /**
+     * @param addressLine2 the addressLine2 to set
+     */
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    /**
+     * @return the postalCode
+     */
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    /**
+     * @param postalCode the postalCode to set
+     */
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    /**
+     * @return the accessRight
+     */
+    public EmployeeAccessRight getAccessRight() {
+        return accessRight;
+    }
+
+    /**
+     * @param accessRight the accessRight to set
+     */
+    public void setAccessRight(EmployeeAccessRight accessRight) {
+        this.accessRight = accessRight;
+    }
     
 }
